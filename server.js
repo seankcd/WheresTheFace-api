e10b195ac1da7c851d238989b45f287e25e7fab2
Sean Courtney <seankc@me.com>
2020-09-03 16:04:59 -0700

    Fix

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = knex({
     connection: {
       host : '127.0.0.1',
       user : 'seank',
-      password : 'bubbles132',
+      password : 'dbpass',
       database : 'wherestheface'
     }
 });
